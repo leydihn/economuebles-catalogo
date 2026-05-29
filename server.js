8de73e7cefb45ab73a315d78e978eba57b39fc5b
Dana <danaleydihernandezsantos@gmail.com>
2026-05-29 10:26:03 -0500

    Uso de variables de entorno para DB

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(express.static('public'));
 
 // Conexión dinámica para Railway
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT || 3306
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306
 });
 
 db.connect((err) => {
